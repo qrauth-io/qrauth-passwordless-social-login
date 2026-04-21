9dc5048eab67a3b1604fd316abd3637c67299396
aristech <aris@progressnet.gr>
2026-04-22 01:48:57 +0300

    author URI update

--- qrauth-passwordless-social-login.php
+++ qrauth-passwordless-social-login.php
@@ -7,7 +7,7 @@
  * Requires at least: 6.4
  * Requires PHP:      8.2
  * Author:            QRAuth
- * Author URI:        https://qrauth.io
+ * Author URI:        https://github.com/aristech
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain:       qrauth-passwordless-social-login
